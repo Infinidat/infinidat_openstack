fcd4c375094d6c35184f38508def3cff6353f236
Arnon Yaari <arnony@infinidat.com>
2015-08-05 12:41:14 +0300

    use new oslo.config import name

--- src/infinidat_openstack/cinder/volume.py
+++ src/infinidat_openstack/cinder/volume.py
@@ -1,4 +1,4 @@
-from oslo.config import cfg
+from oslo_config import cfg
 try:
     from cinder.openstack.common import log as logging
     from cinder.openstack.common.gettextutils import _ as translate

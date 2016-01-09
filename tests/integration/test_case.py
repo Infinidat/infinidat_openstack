ff3f0dd3d815db329149de9a9e6ede34fff61fce
Guy Rozendorn <guy@rzn.co.il>
2016-01-09 22:37:14 +0200

    TRIVIAL fixing OpenStackISCSITestCase._install_scst_for_current_kernel_or_skip_test

--- tests/integration/test_case.py
+++ tests/integration/test_case.py
@@ -643,7 +643,7 @@ class OpenStackISCSITestCase(OpenStackTestCase):
     @classmethod
     def _install_scst_for_current_kernel_or_skip_test(cls):
         execute_assert_success("yum install -y svn || apt-get install -y subversion", shell=True)
-        execute_assert_success("yum install -y kernel-devel-`uname -r` || apt-get install -y linux-headers-`uname -r", shell=True)
+        execute_assert_success("yum install -y kernel-devel-`uname -r` || apt-get install -y linux-headers-`uname -r`", shell=True)
         execute_assert_success("svn checkout svn://svn.code.sf.net/p/scst/svn/trunk scst-trunk", shell=True)
         execute_assert_success("cd scst-trunk/scst && make && make install && modprobe scst && modprobe scst_raid scst_disk", shell=True)
         execute_assert_success("cd scst-trunk/iscsi-scst && make && make install")

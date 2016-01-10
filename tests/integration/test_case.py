afee1e367b2b0b573022aee033958aa3fcbae75a
Guy Rozendorn <guy@rzn.co.il>
2016-01-10 16:50:53 +0200

    TRIVIAL trying to make tests pass

--- tests/integration/test_case.py
+++ tests/integration/test_case.py
@@ -658,9 +658,8 @@ class OpenStackISCSITestCase(OpenStackTestCase):
         from os import remove
         for filepath in list(glob("/etc/yum.repos.d/*iscsi*")):
             remove(filepath)
-        execute("curl http://repo.lab.il.infinidat.com/setup/iscsi-gateway-develop | sudo sh -", shell=True)
         cls._install_scst_for_current_kernel_or_skip_test()
-        execute_assert_success(["yum", "makecache"])
+        execute("curl http://repo.lab.il.infinidat.com/setup/iscsi-gateway-develop | sudo sh -", shell=True)
         logger.debug(execute_assert_success(["yum", "install", "-y", "iscsi-manager"]).get_stdout())
         if path.exists("/etc/init.d/tgtd"): # does not exist on redhat-7
             execute(["/etc/init.d/tgtd", "stop"])

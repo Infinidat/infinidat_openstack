dbcfe437ca74cc201e3fc59f468c3e96f5148d7a
Arnon Yaari <arnony@infinidat.com>
2016-11-08 12:55:41 +0200

    remove infi_requests, we don't remember why we needed it and it makes the tests fail

--- tests/bdist_rpm/build.py
+++ tests/bdist_rpm/build.py
@@ -59,7 +59,6 @@ def main():
     --build-script=tests/bdist_rpm/_build_script.sh \
     --vendor Infinidat --packager Infinidat """
 
-    os.system("projector requirements add infi_requests")
     os.system("projector devenv build --use-isolated-python")
     change_version_in_setup_py()
     try:
